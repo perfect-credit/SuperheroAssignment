f13a03dc765316d5eb892b4ffb553ec88bac1527
perfect-credit <185212043+perfect-credit@users.noreply.github.com>
2025-01-31 00:53:31 +0900

    fix:allow cors on backend

--- superhero-backend/src/main.ts
+++ superhero-backend/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Superhero API')
+    .setDescription('The Superhero API description')
+    .setVersion('1.0')
+    .addTag('superheroes')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
